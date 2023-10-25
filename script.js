a2bd1f70b0adf615879a13fcbea6b3aab1f436cd
josueEstrada-94 <josue.estrada.94@gmail.com>
2023-10-25 13:56:10 -0600

    Game completed. 5 rounds and the function also returns a win/lose/tie result

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     //Choices array
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
   
     // Random indexing
     let randomIndex = Math.floor(Math.random() * choices.length);
@@ -10,20 +10,64 @@ function getComputerChoice() {
   }
   
 function playRound(playerSelection, computerSelection){
-    
+    // This function returns a string if the player win or not against the computer answer. 
+    //This is just a single round.
+
     if (playerSelection === computerSelection)
     {
-      console.log('This is a tie!')
+      return'This is a tie!'
 
-    } else if (playerSelection === 'paper' && computerSelection === 'Rock') 
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') 
     {
-      console.log('you won!')
-    } else{
-      console.log('you lose')
+      return'you won! paper beats rock'
+    } 
+    
+    else if (playerSelection === 'rock' && computerSelection === 'scissors')
+    {
+      return'you won! rock beats scissors'
+    }
+    
+    else if (playerSelection === 'scissors' && computerSelection === 'paper')
+    {
+      return'you won! scissors beats paper'
+    }
+
+    else
+    {
+      return'you lose!'
     }
 }
 
-const playerSelection = 'Paper';
-const computerSelection = getComputerChoice();
+    // Setting the conditions to decide who won the game.
+    function game(){
+      let playerScore = 0;
+      let computerScore = 0;
+    
+      for (let i = 0; i < 5; i++) {
+        // Declaring the variables.
+        const playerSelection = prompt('Select rock, paper or scissors').toLowerCase()
+        const computerSelection = getComputerChoice();
+    
+        let result = playRound(playerSelection, computerSelection)
+    
+        // Showing the round and the result of the round. 
+        console.log(`Round ${i + 1}: ${result}`);
+    
+        // Setting the conditions to gain score.
+        if (result.includes('you won')) {
+          playerScore++;
+        } else if (result.includes('you lose')) {
+          computerScore++;
+        }
+      }
+        // Setting the conditions to decide who won the game.
+        if (playerScore > computerScore) {
+          console.log('You won the game!');
+        } else if (playerScore === computerScore) {
+          console.log('It\'s a tie!');
+        } else {
+          console.log('You lose!');
+        }
+}
 
-console.log(playRound(playerSelection, computerSelection))
+game()
